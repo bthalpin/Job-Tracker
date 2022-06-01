3e9f5a9be24c2f84184f7565f3e944ab0bd0bd21
bthalpin <brian.t.halpin@gmail.com>
2022-05-31 22:17:08 -0400

    Prevent error on login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
 import Auth from '../../utils/auth';
+import './login.css';
 
 function Login() {
     const navigate = useNavigate()
+    const [errorMessage,setErrorMessage] = useState('')
+    const [loginOrRegister,setLoginOrRegister] = useState('login')
     const [user,setUser] = useState({
         name:'',
         email:'',
@@ -12,6 +15,9 @@ function Login() {
     
     const register = (e) => {
         e.preventDefault();
+        if (user.name===''||user.email===''||user.password===''){
+            return
+        }
         fetch('/user',{
             method:'POST',
             headers:{
@@ -21,12 +27,30 @@ function Login() {
 
         }).then(response=>response.json())
         .then(data=>{
+            if (!data.token){
+                setErrorMessage('invalid')
+                return
+            }
             Auth.login(data.token)
+            setErrorMessage('')
             navigate('/home/')
+        })
+    }
+
+    const changeLogin = (path)=>{
+        setLoginOrRegister(path)
+        setErrorMessage('')
+        setUser({
+            name:'',
+            email:'',
+            password:''
         })
     }
     const login = (e) => {
         e.preventDefault();
+        if (user.email===''||user.password===''){
+            return
+        }
         fetch('/user/login',{
             method:'POST',
             headers:{
@@ -36,24 +60,54 @@ function Login() {
 
         }).then(response=>response.json())
         .then(data=>{
+            
+            if (!data.token){
+                setErrorMessage('invalid')
+                return
+            }
             Auth.login(data.token)
+            setErrorMessage('')
             navigate('/home/')
         })
 
     }
     return (
         <>
-            <form onSubmit={register}>
-                <input name="name" type="text" value={user.name} onChange={(e)=>setUser({...user,name:e.target.value})} placeholder="Enter Name"></input>
-                <input name="email" type="text" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} placeholder="Enter Email"></input>
-                <input name="password" type="text" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} placeholder="Enter Password"></input>
-                <button>Register</button>
-            </form>
-            <form onSubmit={login}>
-                <input name="email" type="text" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} placeholder="Enter Email"></input>
-                <input name="password" type="text" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} placeholder="Enter Password"></input>
-                <button>Login</button>
-            </form>
+        {loginOrRegister==='register'?
+            <div className="loginContainer">
+                <form onSubmit={register}>
+                    <div className="inputContainer">
+                        <label htmlFor="name">Name:</label>
+                        <input name="name" type="text" value={user.name} onChange={(e)=>setUser({...user,name:e.target.value})} placeholder="Enter Name"></input>
+                        <label htmlFor="email">Email:</label>
+                        <input name="email" type="text" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} placeholder="Enter Email"></input>
+                        <label htmlFor="password">Password:</label>
+                        <input name="password" type="text" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} placeholder="Enter Password"></input>
+                    
+                    </div>
+                    <button className="loginButton">Register</button>
+                    
+                </form>
+                <button onClick={()=>changeLogin('login')}>Return to Login</button>
+                {errorMessage}
+            </div>
+        
+        :   
+            <div className="loginContainer">
+                <form onSubmit={login}>
+                    <div className="inputContainer">
+                        <label htmlFor="email">Email:</label>
+                        <input name="email" type="text" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} placeholder="Enter Email"></input>
+                        <label htmlFor="password">Password:</label>
+                        <input name="password" type="text" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} placeholder="Enter Password"></input>
+                    </div>
+                    <button className="loginButton">Login</button>
+                </form>
+                <button onClick={()=>changeLogin('register')}>Click to Register</button>
+                {errorMessage}
+                
+            </div>
+        }
         </>
     );
 }
